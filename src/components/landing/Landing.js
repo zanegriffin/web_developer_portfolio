cb1dc0cac733195185fe2f4ff1ac489b472e38d7
Zane <zanebgriffin@gmail.com>
2021-03-16 19:34:49 -0500

    setting up background curves

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import image from '../../images/landing_image.jpg'
+import './Landing.scss'
+
+const Component = () => {
+    return(
+        <div className='landing'>
+            <img src={image}></img>
+        </div>
+    )
+}
+
+export default Component
